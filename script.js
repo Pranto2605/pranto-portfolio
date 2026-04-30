5f65ff645347e53b5685ecce15974e1f39c29751
Pranto2605 <tayabhasanpranto@gmail.com>
2026-04-30 08:51:22 +0600

    Add files via upload

--- script.js
+++ script.js
@@ -2,3 +2,13 @@ function toggleMenu() {
   const navLinks = document.getElementById("navLinks");
   navLinks.classList.toggle("active");
 }
+
+// Close mobile menu after clicking a link
+const links = document.querySelectorAll(".nav-links a");
+
+links.forEach((link) => {
+  link.addEventListener("click", () => {
+    const navLinks = document.getElementById("navLinks");
+    navLinks.classList.remove("active");
+  });
+});
